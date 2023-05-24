cd7c28e84b19c41286cd01d6e89b12564ec9740d
John Peterson <johnp5419@gmail.com>
2023-05-23 23:14:38 -0600

    Setting up shaders and a compute example.

---
+++ compile.bat
@@ -0,0 +1,4 @@
+C:/VulkanSDK/1.3.239.0/Bin/glslc.exe C:/dev/ColdOpen/src/shaders/shader.vert -o C:/dev/ColdOpen/src/shaders/vert.spv
+C:/VulkanSDK/1.3.239.0/Bin/glslc.exe C:/dev/ColdOpen/src/shaders/shader.frag -o C:/dev/ColdOpen/src/shaders/frag.spv
+C:/VulkanSDK/1.3.239.0/Bin/glslc.exe C:/dev/ColdOpen/src/shaders/shader.comp -o C:/dev/ColdOpen/src/shaders/comp.spv
+pause
